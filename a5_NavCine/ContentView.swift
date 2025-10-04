f10e6bc645a7ecd0777aa6a64ba5c52503d202de
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-10-03 18:19:57 -0600

    Initial Commit

---
+++ a5_NavCine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  a5_NavCine
+//
+//  Created by WIN603 on 03/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ a5_NavCine/a5_NavCineApp.swift
@@ -0,0 +1,17 @@
+//
+//  a5_NavCineApp.swift
+//  a5_NavCine
+//
+//  Created by WIN603 on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct a5_NavCineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
